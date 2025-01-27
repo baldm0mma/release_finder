e7838d8e86196d7944c10928025cb0f80515971e
jev forsberg <jev.forsberg@grafana.com>
2025-01-27 15:47:54 -0700

    main/ swicth dirs, fetch tags, and error handle

--- main.go
+++ main.go
@@ -8,5 +8,24 @@ type Release struct {
 }
 
 func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: release-finder <grafana-repo-path> <commit-hash>")
+		os.Exit(1)
+	}
+
+	repoPath := os.Args[1]
+	commitHash := os.Args[2]
+
+	// Change to the repository directory for git commands
+	if err := os.Chdir(repoPath); err != nil {
+		fmt.Println("Error: Could not change to repository directory: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := exec.Command("git", "fetch", "--prune", "--tags", "--force", "origin").Run(); err != nil {
+		fmt.Println("Error fetching latest tags: %v\n", err)
+		os.Exit(1)
+	}
+
 	
 }
